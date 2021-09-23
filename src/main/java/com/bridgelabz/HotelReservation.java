b9bd45bb2e5b6cdfeb97f716735a6db3e42e2376
sreeliptaa <sreelipta65@gmail.com>
2021-09-23 08:27:30 +0530

    Added: welcome message

---
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - To build a Hotel Reservation System
+ * Author - Sreelipta
+ * @since - 2021-09-23
+ */
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
