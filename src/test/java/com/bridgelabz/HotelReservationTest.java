57eae8670a81602d5c6a245f6a4027d536dd21ef
sreeliptaa <86236489+sreeliptaa@users.noreply.github.com>
2021-09-23 10:30:32 +0530

    Added: method for adding details of hotel and test cases

--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -4,25 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
-    HotelReservation hotelReservation = new HotelReservation();
-    Hotel detailOfHotel1 = hotelReservation.addHotel("Lakewood", 110);
-    Hotel detailOfHotel2 = hotelReservation.addHotel("Bridgewood", 160);
-    Hotel detailOfHotel3 = hotelReservation.addHotel("Ridgewood", 220);
+     HotelReservation hotelReservation = new HotelReservation();
 
     @Test
-    void whenAddedHotelName_ShouldReturn_EnteredHotelName() {
-        Assertions.assertEquals(detailOfHotel1.getHotelName(), "Lakewood");
-        Assertions.assertEquals(detailOfHotel2.getHotelName(), "Bridgewood");
-        Assertions.assertEquals(detailOfHotel3.getHotelName(), "Ridgewood");
-
+    public void whenAddedHotel_ShouldReturn_AddedHotelDetails() {
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("LakeWood", 110)));
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("Bridgewood", 160)));
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("Ridgewood", 220)));
     }
-
-    @Test
-    void whenAddedWeekdayRate_ShouldReturn_EnteredWeekdayRate() {
-        Assertions.assertEquals(detailOfHotel1.getWeekdayRate(), 110);
-        Assertions.assertEquals(detailOfHotel2.getWeekdayRate(), 160);
-        Assertions.assertEquals(detailOfHotel3.getWeekdayRate(), 220);
-
-    }
-
 }
